09232aa0ee78a75077aadee9f56c903583022425
winder2412 <winderdawshmaster@gmail.com>
2024-02-27 05:46:55 +0330

    changing from dwm to hyprland

--- configuration.nix
+++ configuration.nix
@@ -2,234 +2,250 @@
 
 {
     nixpkgs.config.allowUnfree = true;
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
-
+    imports =
+        [
+        /etc/nixos/hardware-configuration.nix
+        ];
 ##  Bootloader
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-# boot.loader.grub.efiSupport = true;
-# boot.loader.grub.efiInstallAsRemovable = true;
-# boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
+    boot = {
+        loader = {
+            grub = {
+                enable = true;
+                device = "nodev";
+                fsIdentifier = "label";
+                efiSupport = true;
+            };
+            efi.canTouchEfiVariables = true;
+        };
+        supportedFilesystems = [ "zfs" ];
+        zfs.extraPools = [ "wpool" ];
+        zfs.forceImportRoot = false;
+    };
 ##  Networking
-    networking.hostName = "nixos";
-   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-    networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-    networking.firewall.checkReversePath = false;
-
+    networking = {
+        hostName = "nixos";
+        hostId = "cad6d24f";
+        networkmanager.enable = true;
+        firewall.enable = false;
+    };
 ##  Set your time zone.
     time.timeZone = "Asia/Tehran";
-
-## proxy
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-##  Services {
-
-## Picom
-  services.picom.enable = true;
-
-## Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-## Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-## Enable WindowManager
-  services.xserver.windowManager.dwm.enable = true;
-  programs.hyprland.enable = true;
-    # overlay
-  nixpkgs.overlays = [
-      (final: prev: {
-       dwm = prev.dwm.overrideAttrs(old: { src = /home/winder/Github/dwm-winder;});
-       hyprland = prev.hyprland.overrideAttrs({ legacyRenderer = true; });
-       })
-  ];
-
-## Enable DisplayManager & AutoLogin
-  services.xserver.displayManager = {
-	lightdm.enable = true;
-  	autoLogin = {
-		enable = true;
-		user = "winder";
-	};
-  };
-
-  services.resolved.enable = true;
-
-  
-## Enable GVFS 
-  services.gvfs.enable = true;
-
+## Services
+    services = {
+## Enable Xserver
+        xserver = {
+            enable = true;
+            displayManager = {
+                sddm = {
+                    enable = true;
+                    wayland.enable = true;
+                    settings = {
+                        Autologin = {
+                            Session = "hyprland";
+                            User = "winder";
+                        };
+                    };
+                };
+            };
 ## Configure keymap in X11
-  services.xserver.layout = "us,ir";
-  services.xserver.xkbOptions = "grp:alt_shift_toggle";
-
-# Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-## Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-## }
-
+            xkb = {
+                layout = "us,ir";
+                options = "grp:alt_shift_toggle";
+            };
+## Enable Nvidia
+            videoDrivers = ["nvidia"];
+        };
+## Enable GVFS
+        gvfs.enable = true;
+## Enable the OpenSSH daemon.
+        openssh.enable = true;
+## Enable Pipewire
+        pipewire = {
+            enable = true;
+            audio.enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+        };
+## Jellyfin
+        jellyfin.enable = true;
+## Sing-box
+        sing-box.enable = true;
+## Resolved
+       resolved.enable = true;
+## flacpack
+       flatpak.enable = true;
+    };
 ## Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+    sound.enable = true;
 
+## Hardware
+    hardware = {
+        opengl = {
+            enable = true;
+            driSupport = true;
+            driSupport32Bit = true;
+        };
+        nvidia = {
+            modesetting.enable = true;
+            powerManagement.enable = false;
+            powerManagement.finegrained = false;
+            open = false;
+            nvidiaSettings = true;
+            package = config.boot.kernelPackages.nvidiaPackages.stable;
+        };
+    };
+# Define a user account.
+    users.users.winder = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+    };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.winder = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  };
-
-  environment.systemPackages = with pkgs; [
-    appimage-run
-    alacritty
-    anydesk
-    arandr
-    autojump
-    bat
-    clang-tools_9
-    cmake
-    dmenu
-    dwm
-    fastfetch
-    feh
-    google-chrome
-    gparted
-    gnumake
-    glibc
-    gcc
-    git
-    gh
-    htop
-    kdeconnect
-    libsForQt5.qt5.qtx11extras
-    libsForQt5.qt5.qtx11extras.dev
-    libsForQt5.qt5.qttools
-    libsForQt5.qt5.qttools.dev
-    libsForQt5.qt5.qtbase
-    libsForQt5.qt5.qtbase.dev
-    libsForQt5.qt5.qtsvg
-    libsForQt5.qt5.qtsvg.dev
-    libyaml
-    libpng
-    lxappearance
-    networkmanagerapplet
-    neovim
-    ntfs3g
-    ninja
-    openjdk17-bootstrap
-    protobuf
-    python3
-    rofi
-    st
-    sshuttle
-    starship
-    telegram-desktop
-    trash-cli
-    tldr
-    tree
-    unzip
-    ventoy-full
-    vlc
-    vim
-    wget
-    xfce.thunar
-    xorg.libXinerama
-    xorg.libX11.dev
-    xorg.libX11
-    xorg.libxcb
-    xorg.libXft
-    xorg.xinit
-    xclip
-    yaml-cpp
-    zxing-cpp
-    zlib
-  ];
-
+## Install Packages
+    environment.systemPackages = with pkgs; [
+            alacritty
+            anydesk
+            appimage-run
+            arandr
+            autojump
+            bat
+            clang-tools_9
+            cmake
+            doctl
+            dmenu
+            dwm
+            fastfetch
+            feh
+            gcc
+            gh
+            git
+            glibc
+            gnumake
+            go
+            google-chrome
+            gparted
+            heroic
+            htop
+            jellyfin
+            jellyfin
+            jellyfin-ffmpeg
+            jellyfin-web
+            kdeconnect
+            kitty
+            libpng
+            libsForQt5.qt5.qtbase
+            libsForQt5.qt5.qtbase.dev
+            libsForQt5.qt5.qtsvg
+            libsForQt5.qt5.qtsvg.dev
+            libsForQt5.qt5.qttools
+            libsForQt5.qt5.qttools.dev
+            libsForQt5.qt5.qtx11extras
+            libsForQt5.qt5.qtx11extras.dev
+            libyaml
+            lxappearance
+            neovim
+            networkmanagerapplet
+            ninja
+            nodejs_21
+            ntfs3g
+            obs-studio
+            openjdk17-bootstrap
+            pavucontrol
+            protobuf
+            python3
+            qpwgraph
+            rofi
+            sing-box
+            sshuttle
+            st
+            starship
+            swww
+            telegram-desktop
+            tldr
+            trash-cli
+            tree
+            unzip
+            ventoy-full
+            vim
+            vlc
+            waybar
+            wget
+            wl-clipboard
+            xorg.libX11
+            xorg.libX11.dev
+            xorg.libXft
+            xorg.libXinerama
+            xorg.libxcb
+            xorg.xinit
+            yaml-cpp
+            zfs
+            zlib
+            zoxide
+            zxing-cpp
+            ];
 
 ## Programs
-
-# Bash settings
-  programs.bash.shellInit = "source /home/winder/.config/bash/bashrc";
-  programs.bash.interactiveShellInit = "source /home/winder/.config/bash/bashrc";
-# Thunar settings
-  programs.thunar.enable = true;
-  programs.thunar.plugins = with pkgs.xfce; [
-      thunar-volman
-  ];
-
-# Fonts Config
-
-fonts = {
-    fonts = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      font-awesome
-      source-han-sans
-      source-han-sans-japanese
-      source-han-serif-japanese
-      (nerdfonts.override { fonts = [ "Meslo" ]; })
-    ];
-    fontconfig = {
-      enable = true;
-      defaultFonts = {
-	      monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
-	      serif = [ "Noto Serif" "Source Han Serif" ];
-	      sansSerif = [ "Noto Sans" "Source Han Sans" ];
-      };
+    programs = {
+## Bash settings
+        bash = {
+            shellInit = "source /home/winder/.config/bash/bashrc";
+            interactiveShellInit = "source /home/winder/.config/bash/bashrc";
+        };
+## Enable WindowManager
+        hyprland.enable = true;
+## Thunar settings
+        thunar = {
+            enable = true;
+            plugins = with pkgs.xfce; [
+                thunar-volman
+            ];
+        };
     };
-};
+# Fonts Config
+    fonts = {
+        packages = with pkgs; [
+            noto-fonts
+                noto-fonts-cjk
+                noto-fonts-emoji
+                font-awesome
+                source-han-sans
+                source-han-sans-japanese
+                source-han-serif-japanese
+                (nerdfonts.override { fonts = [ "Meslo" ]; })
+        ];
+        fontconfig = {
+            enable = true;
+            defaultFonts = {
+                monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+                serif = [ "Noto Serif" "Source Han Serif" ];
+                sansSerif = [ "Noto Sans" "Source Han Sans" ];
+            };
+        };
+    };
 
 ## Polkit configs
-  security.polkit.enable = true;
- systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-  };
-   extraConfig = ''
-     DefaultTimeoutStopSec=10s
-   '';
-};
-
-## Firewall Settings
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+    security.polkit.enable = true;
+    systemd = {
+        user.services.polkit-gnome-authentication-agent-1 = {
+            description = "polkit-gnome-authentication-agent-1";
+            wantedBy = [ "graphical-session.target" ];
+            wants = [ "graphical-session.target" ];
+            after = [ "graphical-session.target" ];
+            serviceConfig = {
+                Type = "simple";
+                ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+                Restart = "on-failure";
+                RestartSec = 1;
+                TimeoutStopSec = 10;
+            };
+        };
+        extraConfig = ''
+            DefaultTimeoutStopSec=10s
+            '';
+    };
+## System
+    system = {
+        copySystemConfiguration = true;
+        stateVersion = "unstable";
+    };
 }
-
