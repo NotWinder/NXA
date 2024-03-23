66dfc6c9c8ccb32f40b9acfa684c3e172e9469ae
winder2412 <winderdawshmaster@gmail.com>
2024-03-23 18:20:44 +0330

    trying to add the legacy render to hyprland laptop

--- nixos/laptop/modules.nix
+++ nixos/laptop/modules.nix
@@ -9,7 +9,7 @@
       ../modules/packages/others.nix
       ../modules/services.nix
       ../modules/wm/display-manager.nix
-      ../modules/wm/hyprland.nix
+      ../modules/wm/hyprland-laptop.nix
       ../modules/wm/polkit.nix
       ../modules/wm/thunar.nix
       ../modules/wm/xserver.nix
--- nixos/modules/wm/hyprland-laptop.nix
+++ nixos/modules/wm/hyprland-laptop.nix
@@ -1,14 +1,11 @@
 { pkgs, inputs, config, username, asztal, ... }: {
 
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
-
   programs.hyprland = {
     enable = true;
+    inputs.hyprland.packages.${pkgs.system}.hyprland = {
+      legacyRenderer = true; # whether to use the legacy renderer (for old GPUs)
+    };
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    xwayland.enable = true;
   };
 
   xdg.portal = {
