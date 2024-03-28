750ba7e8d7b5a3233079a65bc336449d6c186b85
winder2412 <winderdawshmaster@gmail.com>
2024-03-28 04:36:54 +0330

    fixing the hyprland version getting overriden by home manager

--- homes/winder/program/graphical/wm/hyprland/default.nix
+++ homes/winder/program/graphical/wm/hyprland/default.nix
@@ -3,6 +3,7 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     settings = {
       "$mainMod" = "SUPER";
       "$browser" = "chromium";
