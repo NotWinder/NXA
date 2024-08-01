ea8364d50efd65f8a1e83b3ce3f5ad5028b90b8d
winder2412 <winderdawshmaster@gmail.com>
2024-08-01 05:03:01 +0330

    neovim transformed to a half lua half nix abomination(practiclly the same thing minus mason installs and lazy-nvim)

--- homes/winder/program/terminal/editors/neovim/default.nix
+++ homes/winder/program/terminal/editors/neovim/default.nix
@@ -5,7 +5,23 @@
         enable = true;
         plugins = with pkgs.vimPlugins; [
             undotree
+            telescope-nvim
+            telescope-ui-select-nvim
+            none-ls-nvim
+            onedark-nvim
+            lualine-nvim
+            harpoon
+            mason-nvim
+            mason-lspconfig-nvim
+            nvim-lspconfig
+            cmp-nvim-lsp
+            luasnip
+            cmp_luasnip
+            friendly-snippets
+            nvim-cmp
+            nvim-treesitter
         ];
+        extraLuaConfig = "require('winder')";
         viAlias = true;
         vimAlias = true;
         vimdiffAlias = true;
@@ -13,17 +29,25 @@
     };
     programs.ripgrep.enable = true;
 
-    home.file."${config.xdg.configHome}/nvim" = {
-        source = ./nvim;
-        recursive = true;
-    };
+   home.file."${config.xdg.configHome}/nvim" = {
+       source = ./nvim;
+       recursive = true;
+   }; 
 
     home.packages = with pkgs;[
+        #lsps
         lua-language-server # lua lsp
         gopls # go lsp
         nil # nix lsp
         rust-analyzer # rust lsp
         nodePackages.typescript-language-server # tsserver lsp
+        #formatters and linters
+        stylua
+        golines
+        isort
+        nixpkgs-fmt
+        black
+        revive
     ];
 
 }
