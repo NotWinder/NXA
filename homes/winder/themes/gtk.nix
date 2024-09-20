c66cabc22dc596d78ac88c93fb200be21502eab0
winder2412 <winderdawshmaster@gmail.com>
2024-09-20 17:37:42 +0330

    removing services from home directory(unused)

--- homes/winder/themes/gtk.nix
+++ homes/winder/themes/gtk.nix
@@ -1,20 +1,20 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 
 {
 
-    gtk={
-        enable = true;
-        iconTheme = {
-            package = pkgs.papirus-icon-theme;
-            name = "Papirus-Dark";
-        };
-        theme = {
-            package = pkgs.tokyonight-gtk-theme;
-            name = "Tokyonight-Dark-BL-LB";
-        };
-        cursorTheme = {
-            name = "⚪ Skyrim by ru5tyshark ⚪";
-            size = 33;
-        };
+  gtk = {
+    enable = true;
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
     };
+    theme = {
+      package = pkgs.tokyonight-gtk-theme;
+      name = "Tokyonight-Dark-BL-LB";
+    };
+    cursorTheme = {
+      name = "⚪ Skyrim by ru5tyshark ⚪";
+      size = 33;
+    };
+  };
 }
