0233306f88f74e904024896f3b0beff809b496ca
winder2412 <winderdawshmaster@gmail.com>
2024-11-14 03:41:07 +0330

    added everything other then security in the system directory

--- hosts/cipher/host.nix
+++ hosts/cipher/host.nix
@@ -22,9 +22,6 @@
       #"/boot".options = defaults;
     };
 
-    nixpkgs.config.allowUnfree = true;
-    nix.settings.experimental-features = ["nix-command" "flakes"];
-
     system.stateVersion = "24.05";
   };
 }
