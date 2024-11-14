172d51c4fffe8cf1958ebb12b9d6586e9dfbb3c7
winder2412 <winderdawshmaster@gmail.com>
2024-11-14 15:19:07 +0330

    some styling changes

--- homes/winder/themes/gtk.nix
+++ homes/winder/themes/gtk.nix
@@ -1,22 +1,7 @@
-#{pkgs, ...}:
-#{
-#  home.packages = [pkgs.dconf];
-#  gtk = {
-#    enable = true;
-#    iconTheme = {
-#      package = pkgs.papirus-icon-theme;
-#      name = "Papirus-Dark";
-#    };
-#    theme = {
-#      package = pkgs.tokyonight-gtk-theme;
-#      name = "Tokyonight-Dark-BL-LB";
-#    };
 #    cursorTheme = {
 #      name = "⚪ Skyrim by ru5tyshark ⚪";
 #      size = 33;
 #    };
-#  };
-#}
 {
   osConfig,
   config,
