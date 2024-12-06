f2e78e6ab1cc0e376f8531af6e56cf3222f5180f
winder2412 <winderdawshmaster@gmail.com>
2024-12-06 08:25:09 +0330

    commenting out some of the security modules since they caused the system to crash when a second monitor was pluged in (which doesn't make any sence cuz some of them weren't being used at all)

--- modules/core/common/system/security/module.nix
+++ modules/core/common/system/security/module.nix
@@ -11,16 +11,16 @@
     ./apparmor.nix # apparmor configuration and policies
     ./auditd.nix # auditd
     ./clamav.nix # clamav antivirus
-    ./fprint.nix # fingerprint driver and login support
+    #./fprint.nix # fingerprint driver and login support
     ./impurity-tracker.nix # find applications that depend on /bin/sh or /usr/bin/env
-    ./kernel.nix # kernel hardening
-    ./memalloc.nix # memory allocator hardening
+    #./kernel.nix # kernel hardening
+    #./memalloc.nix # memory allocator hardening
     ./pam.nix # pam configuration
     ./pki.nix # pki certificate bundles
     ./polkit.nix # polkit configuration
     ./selinux.nix # selinux support + kernel patches
     ./sudo.nix # sudo rules and configuration
     ./virtualization.nix # hypervisor hardening
-    ./usbguard.nix # usbguard
+    # ./usbguard.nix # usbguard
   ];
 }
