c9db2a1209826c52539263d6fcfc8319a96623fa
winder2412 <winderdawshmaster@gmail.com>
2024-12-07 06:42:22 +0330

    removing some of the unnesesery packages from others.nix (will move everyting to aproprate location later)

--- modules/core/common/system/os/programs/others.nix
+++ modules/core/common/system/os/programs/others.nix
@@ -1,45 +1,25 @@
 {pkgs, ...}: {
   ## Install Packages
   environment.systemPackages = with pkgs; [
-    #davinci-resolve
     anydesk
     bottles
     celluloid
-    cobra-cli
-    discord
-    doctl
-    gcc
-    gh
     gparted
     heroic
-    jq
-    kdePackages.dolphin
-    kdePackages.dolphin-plugins
-    kdePackages.kget
-    mangohud
-    motrix
-    mpv
-    networkmanagerapplet
     nwg-displays
     ntfs3g
     pcsx2
     picard
     picard-tools
-    qbittorrent
     qpwgraph
     sing-box
-    sshuttle
     telegram-desktop
     tldr
     tokei
-    trash-cli
-    tree
     uget
     universal-android-debloater
     unrar
-    unzip
     ventoy
     vlc
-    wget
   ];
 }
