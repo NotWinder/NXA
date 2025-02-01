03dcb4e38264c0789d9f88db7a9974d97c9fdb5d
winder2412 <winderdawshmaster@gmail.com>
2025-02-01 21:04:36 +0330

    update feb 1st

--- hosts/cipher/fs/default.nix
+++ hosts/cipher/fs/default.nix
@@ -2,24 +2,24 @@
   config = {
     fileSystems = {
       "/boot" = {
-        device = "/dev/disk/by-uuid/D619-D553";
+        device = "/dev/disk/by-uuid/3105-B2F4";
         fsType = "vfat";
       };
 
       "/" = {
-        device = "/dev/disk/by-uuid/55b0f02e-8949-42e1-86ad-60559647f1fd";
+        device = "/dev/disk/by-uuid/f2ff3005-0933-4fb5-96d9-78951f808f5b";
         fsType = "btrfs";
         options = ["subvol=root" "compress=zstd" "noatime"];
       };
 
       "/nix" = {
-        device = "/dev/disk/by-uuid/55b0f02e-8949-42e1-86ad-60559647f1fd";
+        device = "/dev/disk/by-uuid/f2ff3005-0933-4fb5-96d9-78951f808f5b";
         fsType = "btrfs";
         options = ["subvol=nix" "compress=zstd" "noatime"];
       };
 
       "/home" = {
-        device = "/dev/disk/by-uuid/55b0f02e-8949-42e1-86ad-60559647f1fd";
+        device = "/dev/disk/by-uuid/f2ff3005-0933-4fb5-96d9-78951f808f5b";
         fsType = "btrfs";
         options = ["subvol=home" "compress=zstd"];
       };
