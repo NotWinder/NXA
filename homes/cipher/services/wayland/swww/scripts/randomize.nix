b16c5bafc54ddf5b031d599d22414a53fa87698d
notwinder <winderdawshmaster@gmail.com>
2025-05-15 20:30:40 +0330

    added transitions to swww and fixed the current_wallpaper var in the bash script

--- homes/cipher/services/wayland/swww/scripts/randomize.nix
+++ homes/cipher/services/wayland/swww/scripts/randomize.nix
@@ -7,14 +7,16 @@
   swww-random = pkgs.writeShellScriptBin "swww-random" ''
     TOP_DIR=${winpaper.wallpkgs}
     WALLPAPER_DIR="$TOP_DIR/share/wallpapers/"
-    CURRENT_WALL=$(swww query)
+    # Capture the output of `swww query`
+    OUTPUT=$(swww query)
+    CURRENT_WALL=$(echo "$OUTPUT" | grep -m1 'image:' | sed 's/.*image: //')
 
     # Get a random wallpaper that is not the current one
     WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
     # Apply the selected wallpaper
     sleep 1
-    swww img "$WALLPAPER"
+    swww img "$WALLPAPER" --transition-fps 60 --transition-type grow --transition-duration 1
   '';
 in {
   home.packages = [swww-random];
