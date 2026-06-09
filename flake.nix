fdc7044d36fbb89e59e2548fd3336ac3f8f8a717
notwinder <notwinder@gmail.com>
2026-06-09 12:00:03 +0330

    stylix: override gnome-shell source to use GitHub codeload
    GitLab's CDN keeps truncating the gnome-shell archive download.
    Override the stylix input to fetch from GitHub's codeload instead,
    which serves the full archive reliably.

--- flake.nix
+++ flake.nix
@@ -48,7 +48,13 @@
 
     stylix = {
       url = "github:nix-community/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        gnome-shell = {
+          url = "https://codeload.github.com/GNOME/gnome-shell/tar.gz/ef02db02bf0ff342734d525b5767814770d85b49";
+          flake = false;
+        };
+      };
     };
 
     hyprland.url = "github:/hyprwm/Hyprland";
